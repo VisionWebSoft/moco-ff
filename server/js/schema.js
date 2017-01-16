b9ea243c26cc8ae15740fd37acc6bd8dc71ae331
Andrew <arhq315@gmail.com>
2017-01-16 15:15:41 -0600

    added schemas
    added schemas for users, units, departments, and contacts

--- server/js/schema.js
+++ server/js/schema.js
@@ -1,7 +1,24 @@
 var mongoose=require('mongoose');
 var schema={};
-//contacts
-//departments
+var collections={};
+var contactSchema=new mongoose.Schema(
+{
+	name:
+	{
+		required:true,
+		trim:true,
+		type:String
+	}
+});
+var departmentSchema=new mongoose.Schema(
+{
+	name:
+	{
+		required:true,
+		trim:true,
+		type:String
+	}
+});
 var itemSchema=new mongoose.Schema(
 {
 	contact:
@@ -36,7 +53,33 @@ var itemSchema=new mongoose.Schema(
 		type:ObjectId
 	}
 });
-//units
-//users
+var unitSchema=new mongoose.Schema(
+{
+	name:
+	{
+		required:true,
+		trim:true,
+		type:String
+	}
+});
+var userSchema=new mongoose.Schema(
+{
+	name:
+	{
+		required:true,
+		trim:true,
+		type:String
+	},
+	password://must encypt this!!
+	{
+		required:true,
+		type:String
+	}
+});
+
+schema.contact=mongoose.model('Contact',contactSchema);
+schema.department=mongoose.model('Department',departmentSchema);
 schema.item=mongoose.model('Item',itemSchema);
+schema.unit=mongoose.model('Unit',unitSchema);
+schema.user=mongoose.model('User',userSchema);
 module.exports=schema;
