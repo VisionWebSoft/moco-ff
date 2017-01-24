487fef8e0c67b6a480d1b395090346a54a8eb9f3
Andrew <arhq315@gmail.com>
2017-01-24 15:35:03 -0600

    refactored newDatabase()
    moved code around for initial setup

--- server/js/output.js
+++ server/js/output.js
@@ -64,24 +64,43 @@ logic.json2mongo=function(arr)
 	}
 };
 global.output={};
-output.newDatabase=function()
+output.newDatabase=function(url)
 {
-	//create items
-	contacts.map(name=>({name})).forEach(obj=>schema.contact.create(obj,(err,obj)=>err?output.error(err):console.log(obj.name)));
-	depts.map(name=>({name})).forEach(obj=>schema.department.create(obj,(err,obj)=>err?output.error(err):console.log(obj.name)));
-	units.map(name=>({name})).forEach(obj=>schema.unit.create(obj,(err,obj)=>err?output.error(err):console.log(obj.name)));
-	logic.json2mongo(logic.getDB());
-	//create users
-	schema.user.pre('save',function(next)
+	mr.freeze(config,input,logic,output);
+	fs.readFile(url+'/data/inventory.csv','utf8',function(err,data)
 	{
-		var user=this;
-		var salt=bcrypt.genSaltSync(10);
-		var hash=bcrypt.hashSync(user.password,salt);
-		user.password=hash;
-		next();
+		if (err)
+		{
+			output.error(err);
+		}
+		else
+		{
+			logic.setDB(data);
+			mongoose.connect('mongodb://localhost:27017/moco-ff');
+			var db=mongoose.connection;
+			db.on('error',output.error);
+			var json=logic.getDB();
+			var contacts=logic.uniqueEntries(json,'Contact Person');
+			var depts=logic.uniqueEntries(json,'Fire Department');//fix redundant data!!
+			var units=logic.uniqueEntries(json,'Unit Number');	
+			//create items
+			contacts.map(name=>({name})).forEach(obj=>schema.contact.create(obj,(err,obj)=>err?output.error(err):console.log(obj.name)));
+			depts.map(name=>({name})).forEach(obj=>schema.department.create(obj,(err,obj)=>err?output.error(err):console.log(obj.name)));
+			units.map(name=>({name})).forEach(obj=>schema.unit.create(obj,(err,obj)=>err?output.error(err):console.log(obj.name)));
+			logic.json2mongo(logic.getDB());
+			//create users
+			schema.user.pre('save',function(next)
+			{
+				var user=this;
+				var salt=bcrypt.genSaltSync(10);
+				var hash=bcrypt.hashSync(user.password,salt);
+				user.password=hash;
+				next();
+			});
+			schema.user.create({user:'user',password:'1longPassPhrase!'},(err,obj)=>err?output.error(err):console.log(obj));
+			schema.user.create({user:'admin',password:'ul7r4%3cur3C0d3!'},(err,obj)=>err?output.error(err):console.log(obj));
+		}
 	});
-	schema.user.create({user:'user',password:'1longPassPhrase!'},(err,obj)=>err?output.error(err):console.log(obj));
-	schema.user.create({user:'admin',password:'ul7r4%3cur3C0d3!'},(err,obj)=>err?output.error(err):console.log(obj));
 };
 output.connect=function()
 {
