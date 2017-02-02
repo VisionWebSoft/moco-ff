efbbecf7a5ff47509f607b75edddb2eae578073e
Andrew <arhq315@gmail.com>
2017-02-01 19:02:16 -0600

    added login form behaviors
    Form hides on authentication and relevant error messages display.

--- client/js/c-input.js
+++ client/js/c-input.js
@@ -8,15 +8,30 @@ input.login=function(event)
 	event.preventDefault();
 	var target=q(event.target);
 	var form=target.parent('form');
+	var password=form.q('.password').val()+'';
+	var user=form.q('.user').val()+'';
 	var obj=
 	{
-		data:
-		{
-			password:form.q('.password').val()+'',
-			user:form.q('.user').val()+''
-		}
+		data:{password,user}//iOS might not be able to handle this!!
 	};
-	output.req('api/login/',obj).then(console.log);
+	if (password.length&&user.length)//optimize these vars!!
+	{
+		output.req('api/login/',obj)
+		.then(function(res)
+		{
+			console.log(res);
+			q('.login__msg').html('');
+			res.auth?q('.login').hide():q('.login__msg').html(res.error);
+		}).catch(output.error);
+	}
+	else
+	{
+		let arr=[];
+		!password.length?arr.push('Enter a password.'):'';
+		!user.length?arr.push('Enter a user name.'):'';
+		q('.login__msg').html(arr.join('<br>'));
+	}
+	
 };
 input.openModal=function(event)
 {
